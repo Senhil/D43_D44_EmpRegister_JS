503d4c86f7dcbb64ef27850d00aa6e2d424b0a70
SK <senthilptj@gmail.com>
2022-04-03 14:59:41 +0530

    D43 Uc1 added

--- JS/payroll_form.js
+++ JS/payroll_form.js
@@ -87,7 +87,6 @@ function save() {
     }
     alert("Employee Added Successfully!\n" + employeePayrollData.toString());
 }
-
 const salary = document.querySelector("#salary");
 const output = document.querySelector(".salary-output");
 salary.oninput = function() {
